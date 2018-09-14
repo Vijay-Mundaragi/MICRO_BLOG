188ddc1f850deec99fd8f85e42872d1f7541ed32
Vijay-Mundaragi <vmundaragi@arubanetworks.com>
2018-09-14 12:42:24 +0530

    edit profile feature

--- my_app/forms.py
+++ my_app/forms.py
@@ -34,3 +34,4 @@ class RegistrationForm(FlaskForm):
 class EditProfileForm(FlaskForm):
 	username = StringField("User Name", validators=[DataRequired()])
 	about_me = TextAreaField("About me:", validators=[Length(min=0, max=140)])
+	submit = SubmitField("Submit")
