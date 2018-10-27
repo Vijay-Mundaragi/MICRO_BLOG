00bebbe58c7cc305d56e4738030f905160bcefe6
Vijay-Mundaragi <saivijay010@gmail.com>
2018-10-27 11:21:25 +0530

    pagination of blog posts

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
 	SECRET_KEY = os.getenv('SECRET_KEY') or 'I am the best...!'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, "ap.db")
 	SQLALCHEMY_TRACK_MODIFICAIONS = False
+	POSTS_PER_PAGE = 3
